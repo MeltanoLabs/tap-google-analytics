342e2d8343528e4351c88353e18ded03f0eb2ff2
pnadolny13 <patnadolny@gmail.com>
2021-11-16 18:06:22 -0500

    test creds as base64 string

--- tap_google_analytics/tests/utilities.py
+++ tap_google_analytics/tests/utilities.py
@@ -1,8 +1,14 @@
 """Test utility funtions."""
+import base64
 import json
 import os
 
 
 def get_secrets_dict():
     """Return a secrets dictionary of based off the env var."""
-    return json.loads(os.environ["CLIENT_SECRETS"])
+    secrets_var = os.environ["CLIENT_SECRETS"]
+    try:
+        secrets_dict = json.loads(secrets_var)
+    except json.JSONDecodeError:
+        secrets_dict = json.loads(base64.b64decode(secrets_var))
+    return secrets_dict
