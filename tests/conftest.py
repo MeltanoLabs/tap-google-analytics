ba35f4d41ba9025ad5d482104e35289606ca7ea2
Ken Payne <ken@meltano.com>
2023-01-26 13:29:57 +0000

    refactor client secret resource

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,3 @@
 """Conftest fixtures."""
 
-import json
-import os
-
-import pytest
-
-from .utilities import get_secrets_dict
-
 pytest_plugins = ("singer_sdk.testing.pytest_plugin",)
-
-
-@pytest.fixture(scope="module")
-def config():
-    """Write secrets file then clean it up after tests."""
-    secrets_path = f"{os.path.dirname(__file__)}/test_data/client_secrets.json"
-    with open(secrets_path, "w") as f:
-        json.dump(get_secrets_dict(), f)
-    yield
-    os.remove(secrets_path)
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,22 +1,14 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import os
 from datetime import datetime, timedelta, timezone
 
-import pytest
 from singer_sdk.testing import get_tap_test_class
 
 from tap_google_analytics.tap import TapGoogleAnalytics
 
-from .utilities import get_secrets_dict
+from .utilities import create_secrets_file, get_secrets_dict
 
-SAMPLE_CONFIG_SERVICE = {
-    "view_id": "188392047",
-    "end_date": datetime.now(timezone.utc).strftime("%Y-%m-%d"),
-    "start_date": (datetime.now(timezone.utc) - timedelta(days=2)).strftime("%Y-%m-%d"),
-    "key_file_location": f"{os.path.dirname(__file__)}/test_data/client_secrets.json",
-}
-
+# Run standard built-in tap tests from the SDK with SAMPLE_CONFIG_CLIENT_SECRETS
 SAMPLE_CONFIG_CLIENT_SECRETS = {
     "view_id": "188392047",
     "end_date": datetime.now(timezone.utc).strftime("%Y-%m-%d"),
@@ -25,25 +17,22 @@ SAMPLE_CONFIG_CLIENT_SECRETS = {
 }
 
 
-# Run standard built-in tap tests from the SDK with SAMPLE_CONFIG_SERVICE
-TapGoogleAnalyticsService = get_tap_test_class(
-    tap_class=TapGoogleAnalytics, config=SAMPLE_CONFIG_SERVICE
-)
-
-
-class TestTapGoogleAnalyticsService(TapGoogleAnalyticsService):
-    @pytest.fixture
-    def resource(self, config):
-        yield
-
-
-# Run standard built-in tap tests from the SDK with SAMPLE_CONFIG_CLIENT_SECRETS
-TapGoogleAnalyticsClientSecrets = get_tap_test_class(
+TestTapGoogleAnalyticsClientSecrets = get_tap_test_class(
     tap_class=TapGoogleAnalytics, config=SAMPLE_CONFIG_CLIENT_SECRETS
 )
 
 
-class TestTapGoogleAnalyticsClientSecrets(TapGoogleAnalyticsClientSecrets):
-    @pytest.fixture
-    def resource(self, config):
-        yield
+with create_secrets_file() as secrets_file_path:
+    # Run standard built-in tap tests from the SDK with SAMPLE_CONFIG_SERVICE
+    SAMPLE_CONFIG_SERVICE = {
+        "view_id": "188392047",
+        "end_date": datetime.now(timezone.utc).strftime("%Y-%m-%d"),
+        "start_date": (datetime.now(timezone.utc) - timedelta(days=2)).strftime(
+            "%Y-%m-%d"
+        ),
+        "key_file_location": secrets_file_path,
+    }
+
+    TestTapGoogleAnalyticsService = get_tap_test_class(
+        tap_class=TapGoogleAnalytics, config=SAMPLE_CONFIG_SERVICE
+    )
